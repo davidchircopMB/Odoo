9d16e4e4de0cb81b1fb891b72461375ffde918e1
davidchircopMB <74653500+davidchircopMB@users.noreply.github.com>
2024-07-01 17:19:17 +0200

    Add files via upload

---
+++ sales_commission_generic/sale/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+from . import sale
+from . import sale_config_settings
+from . import product_template
+from . import designer_commission
