79a8d5cedc069bc35d3de478949d25c20b7e988c
davidchircopMB <74653500+davidchircopMB@users.noreply.github.com>
2024-07-03 15:09:18 +0200

    Delete sales_commission_generic directory

--- sales_commission_generic/sale/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
-
-from . import sale
-from . import sale_config_settings
-from . import product_template
-from . import designer_commission
